2ea1cd10fcee103521be39cd204e49a76ff1d319
Admin <Admin@DESKTOP-CMUK5PH>
2021-10-19 18:42:05 +0530

    debug

---
+++ Dxc/src/Basics/DebugDemo.java
@@ -0,0 +1,23 @@
+package Basics;
+
+public class DebugDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("DebugDemo-- main");
+			Calculator calc = new Calculator();
+			int sum = calc.add(10, 20);
+			System.out.println("the sum is--"+sum);
+			
+			for(int a=0; a<5; a++) {
+				int f = a+ 5 *3;
+				//System.out.println("the no is="+f);
+				
+			}
+			//throw new NullPointerException();
+		}
+
+	
+	
+
+}
