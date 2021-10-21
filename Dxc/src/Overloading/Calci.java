4f03382448d8426e7df76d2d7607a932b82e76b7
Admin <Admin@DESKTOP-CMUK5PH>
2021-10-21 11:10:55 +0530

    method overloading

---
+++ Dxc/src/Overloading/Calci.java
@@ -0,0 +1,18 @@
+package Overloading;
+
+public class Calci {
+      void sum(int a,int b)
+       {
+    	   System.out.println(a+b); 
+       }
+      void sum(int a,long b,double c) {
+    	  System.out.println(a+b+c);
+      }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           Calci cl=new Calci();
+           cl.sum(23,45);
+           cl.sum(2,34556,1234566787);
+	}
+
+}
